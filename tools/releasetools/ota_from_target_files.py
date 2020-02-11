291444cdd37bfd9e6ef7f62d6a7890aac9b23454
LordShenron <shen.priyanshu@gmail.com>
2020-02-11 13:37:38 +0000

    Sakura: use a small art
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -1076,15 +1076,12 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   # the update of the system partition takes the remaining progress.
   system_progress = 0.9 - (len(block_diff_dict) - 1) * 0.1
 
-  script.Print("============================================================");
-  script.Print("   ___             _         __  ____     __                ");
-  script.Print("  / _ \_______    (_)__ ____/ /_/ __/__ _/ /____ _________ _");
-  script.Print(" / ___/ __/ _ \  / / -_) __/ __/\ \/ _ `/  '_/ // / __/ _ `/");
-  script.Print("/_/  /_/  \___/_/ /\__/\__/\__/___/\_,_/_/\_\\_,_/_/  \_,_/ ");
-  script.Print("             |___/                                          ");
-  script.Print("============================================================");
-  script.Print("               By: @ChaitanayaMittal && @LordShenron        ");
-  script.Print("============================================================");
+  script.Print("==============================");
+  script.Print("========PROJECT SAKURA========");
+  script.Print("======BASED ON LINEAGE OS=====");
+  script.Print("=============================");
+  script.Print("     By: @LordShenron        ");
+  script.Print("=============================");
   androidver = target_info.GetBuildProp("ro.build.version.release")
   buildidn = target_info.GetBuildProp("ro.build.id")
   buildday = target_info.GetBuildProp("ro.build.date")
